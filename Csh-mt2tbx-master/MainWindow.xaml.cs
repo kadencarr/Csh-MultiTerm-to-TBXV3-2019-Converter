dedeae652f3b8f475f6d65a8bb94702cc68e6f29
kadencarr <b.kaden.carr@gmail.com>
2018-09-08 07:26:08 -0700

    Updates to TBXV3 Output, Added back UI

---
+++ Csh-mt2tbx-master/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Csh_mt2tbx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string mappingFn = "";
+        public string xmlFn = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mappingUpload_Click(object sender, RoutedEventArgs e)
+        {
+            mappingFn = Methods.readFile("Mapping");
+        }
+
+        private void xmlUpload_Click(object sender, RoutedEventArgs e)
+        {
+            xmlFn = Methods.readFile("XML");
+        }
+
+        private void convert_Click(object sender, RoutedEventArgs e)
+        {
+            if (Core.IsChecked == true)
+            {
+                Methods m = new Methods();
+                m.deserializeFile(mappingFn, xmlFn, "TBX-Core");
+            }
+            else if (Min.IsChecked == true)
+            {
+                Methods m = new Methods();
+                m.deserializeFile(mappingFn, xmlFn, "TBX-Core");
+            }
+            else if (Basic.IsChecked == true)
+            {
+                Methods m = new Methods();
+                m.deserializeFile(mappingFn, xmlFn, "TBX-Core");
+            }
+            this.Close();
+        }
+    }
+}
---
+++ Csh-mt2tbx-master/README.md
@@ -0,0 +1,2 @@
+# Csh-mt2tbx
+This is the C# port of the original mt2tbx converter found here: https://github.com/byutrg/p5-mt2tbx. It uses a mapping file and an XML glossary export from MultiTerm.  This project is not affiliated with SDL in any way.
